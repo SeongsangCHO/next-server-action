5fe097625fa38072e289423182395f8dacba6357
SeongsangCHO <liasqui31@gmail.com>
2023-06-21 00:59:01 +0900

    express, mysql 연동

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,25 @@
 import express, { Request, Response } from "express";
-import { createServer } from "http";
 import next from "next";
-
+import mysql from "mysql2";
 const dev = process.env.NODE_ENV === "development";
 const port = 3000;
 const app = next({ dev, port });
+
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "1234", //Todo env
+  database: "Post",
+  port: 3306,
+});
+
+connection.connect();
+
+connection.query("SELECT * FROM Posts", (err, rows, fields) => {
+  if (err) throw err;
+
+  console.log("Post is: ", rows);
+});
 
 const handle = app.getRequestHandler();
 
